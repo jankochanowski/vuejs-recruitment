e22f5b107cebd92f155f0fa7ecc095db6eefc2c0
jankochanowski <45302044+jankochanowski@users.noreply.github.com>
2019-11-12 22:26:08 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,12 +1,8 @@
-import Vue from "vue";
-import App from "./App.vue";
+import Vue from 'vue'
+import App from './App.vue'
 
-Vue.config.productionTip = false;
+Vue.config.productionTip = false
 
 new Vue({
-  el: "#app",
-  components: {
-    App
-  },
-  template: "<App />"
-});
+  render: h => h(App)
+}).$mount('#app')
